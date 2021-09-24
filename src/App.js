147ed7413f702bf2bb853a2b846dc37fbf581195
Sami <s.m.sami125@gmail.com>
2021-09-24 23:54:48 +0500

    drawer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import * as React from "react";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import ProTip from "./ProTip";
-import About from "./pages/about";
+import React, { useState } from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import ProTip from './ProTip';
+import About from './pages/about';
 import {
   Grid,
   Paper,
@@ -10,45 +10,46 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
-} from "@mui/material";
-import Skills from "./pages/skills";
-import Project from "./pages/project";
-import AppBar from "./pages/AppBar";
+} from '@mui/material';
+import Skills from './pages/skills';
+import Project from './pages/project';
+import AppBar from './pages/AppBar';
 
-import profile from "./assets/profile.jpg";
-import logo from "./assets/ed.png";
-import map from "./assets/map.png";
-import hand from "./assets/hand.gif";
-import location from "./assets/location.jpg";
-import book from "./assets/book.png";
+import profile from './assets/profile.jpg';
+import logo from './assets/ed.png';
+import map from './assets/map.png';
+import hand from './assets/hand.gif';
+import location from './assets/location.jpg';
+import book from './assets/book.png';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-  Link
-} from "react-router-dom";
+  Link,
+} from 'react-router-dom';
 
 export default function App() {
   const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down('md'));
+  const [openDrawer, setOpenDrawer] = useState(false);
 
   return (
-    <Grid container style={{ backgroundColor: "#F6F5FA", minHeight: "100vh" }}>
+    <Grid container style={{ backgroundColor: '#F6F5FA', minHeight: '100vh' }}>
       {/* left bar */}
       <Grid item lg={3} xs={12}>
         <Grid
           container
-          direction={"column"}
-          alignItems={matchesSM ? "center" : undefined}
+          direction={'column'}
+          alignItems={matchesSM ? 'center' : undefined}
         >
           {/* logo */}
           <div
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
             <img src={logo} />
@@ -57,41 +58,41 @@ export default function App() {
           {/* left div */}
           <div
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-              width: "100%",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              width: '100%',
             }}
           >
             <Grid
               container
-              direction={matchesSM ? "row" : "column"}
+              direction={matchesSM ? 'row' : 'column'}
               elevation={0}
               style={{
                 borderRadius: 20,
-                paddingBottom: "1em",
-                boxShadow: "rgba(100,100,111,0.2) 0px 7px 29px 0px",
+                paddingBottom: '1em',
+                boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
                 //boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                width: matchesSM ? "90%" : "50%",
-                marginTop: "2em",
-                backgroundColor: "#F6F5FA",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
+                width: matchesSM ? '90%' : '50%',
+                marginTop: '2em',
+                backgroundColor: '#F6F5FA',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
               }}
             >
               <Grid item>
-                {" "}
+                {' '}
                 {/* first image div */}
                 <div
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    margin: "1em 0px",
-                    overflow: "hidden",
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    margin: '1em 0px',
+                    overflow: 'hidden',
                   }}
                 >
                   <span>
@@ -100,32 +101,32 @@ export default function App() {
                       style={{
                         // border: "3px solid white",
                         // borderRadius: 20,
-                        width: "50px",
-                        height: "50px",
+                        width: '50px',
+                        height: '50px',
                       }}
                     />
                   </span>
                   <img
                     src={profile}
                     style={{
-                      border: "3px solid white",
+                      border: '3px solid white',
                       borderRadius: 20,
-                      width: "100px",
-                      height: "85px",
+                      width: '100px',
+                      height: '85px',
                     }}
                   />
                 </div>
               </Grid>
               <Grid item>
-                {" "}
+                {' '}
                 {/* second image div */}
                 <div
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "start",
-                    margin: "1em 0px",
-                    overflow: "hidden",
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'start',
+                    margin: '1em 0px',
+                    overflow: 'hidden',
                   }}
                 >
                   <span>
@@ -134,32 +135,32 @@ export default function App() {
                       style={{
                         // border: "3px solid white",
                         // borderRadius: 20,
-                        width: "50px",
-                        height: "50px",
+                        width: '50px',
+                        height: '50px',
                       }}
                     />
                   </span>
                   <img
                     src={map}
                     style={{
-                      border: "3px solid white",
+                      border: '3px solid white',
                       borderRadius: 20,
-                      width: "100px",
-                      height: "85px",
+                      width: '100px',
+                      height: '85px',
                     }}
                   />
                 </div>
               </Grid>
               <Grid item>
-                {" "}
+                {' '}
                 {/* third image div */}
                 <div
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "start",
-                    margin: "1em 0px",
-                    overflow: "hidden",
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'start',
+                    margin: '1em 0px',
+                    overflow: 'hidden',
                   }}
                 >
                   <span>
@@ -168,9 +169,9 @@ export default function App() {
                       style={{
                         // border: "3px solid white",
                         // borderRadius: 20,
-                        width: "30px",
-                        height: "30px",
-                        marginTop: "1em",
+                        width: '30px',
+                        height: '30px',
+                        marginTop: '1em',
                       }}
                     />
                   </span>
@@ -178,14 +179,14 @@ export default function App() {
                   <div
                     style={{
                       // border: "3px solid white",
-                      backgroundColor: "#EDF2F8",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
+                      backgroundColor: '#EDF2F8',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
                       borderRadius: 20,
-                      flexDirection: "column",
-                      width: "120px",
-                      height: "70px",
+                      flexDirection: 'column',
+                      width: '120px',
+                      height: '70px',
                     }}
                   >
                     <Typography variant="body1">hello</Typography>
@@ -200,24 +201,24 @@ export default function App() {
                 {/* fourth image div */}
                 <div
                   style={{
-                    margin: "1em 0px",
+                    margin: '1em 0px',
                     borderRadius: 20,
-                    overflow: "hidden",
+                    overflow: 'hidden',
                     // border: "3px solid white",
-                    backgroundColor: "#EDF2F8",
-                    width: "130px",
-                    height: "85px",
+                    backgroundColor: '#EDF2F8',
+                    width: '130px',
+                    height: '85px',
                   }}
                 >
                   {/* <img src={map} style={{ width: "100px", height: "85px" }} /> */}
-                </div>{" "}
+                </div>{' '}
               </Grid>
               <Grid item>
-                {" "}
+                {' '}
                 {/* bottom button */}
                 <div
                   style={{
-                    marginTop: "1em",
+                    marginTop: '1em',
                     // borderRadius: 20,
                     // overflow: "hidden",
                     // border: "3px solid white"
@@ -225,16 +226,16 @@ export default function App() {
                 >
                   <Button
                     style={{
-                      backgroundColor: "white",
-                      border: "1px solid gray",
+                      backgroundColor: 'white',
+                      border: '1px solid gray',
                     }}
                   >
                     <Typography
                       variant="body1"
                       style={{
-                        fontSize: "0.8em",
-                        color: "black",
-                        textTransform: "capitalize",
+                        fontSize: '0.8em',
+                        color: 'black',
+                        textTransform: 'capitalize',
                       }}
                     >
                       View Resume
@@ -248,22 +249,17 @@ export default function App() {
       </Grid>
 
       {/* right bar */}
-      <Grid
-        item
-        lg={9}
-      >
-
-        <AppBar />
+      <Grid item lg={9}>
+        <AppBar openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
 
         <div>
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
-
             <Route path="/" exact>
               <Redirect
                 to={{
-                  pathname: "/about",
+                  pathname: '/about',
                   // state: { from: location }
                 }}
               />
@@ -282,13 +278,10 @@ export default function App() {
             </Route>
 
             <Route path="*">
-              <h1>
-                not found
-              </h1>
+              <h1>not found</h1>
             </Route>
           </Switch>
         </div>
-
       </Grid>
     </Grid>
   );
--- src/pages/AppBar.js
+++ src/pages/AppBar.js
@@ -1,30 +1,87 @@
-import React from "react";
-import { Card, Grid, Typography, useTheme, useMediaQuery } from "@mui/material";
-
+import React from 'react';
+import {
+  Card,
+  Grid,
+  Typography,
+  useMediaQuery,
+  SwipeableDrawer,
+  useTheme,
+  IconButton,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import {
   // BrowserRouter as Router,
   // Switch,
   // Route,
-  Link
-} from "react-router-dom";
+  Link,
+} from 'react-router-dom';
 
-import Man from "../assets/man.jpg";
-export default function AppBar() {
+import Man from '../assets/man.jpg';
+
+export default function AppBar(props) {
   const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const mobile = (
+    <SwipeableDrawer
+      disableBackdropTransition={!iOS}
+      disableDiscovery={iOS}
+      open={props.openDrawer}
+      onClose={() => props.setOpenDrawer(false)}
+      onOpen={() => props.setOpenDrawer(true)}
+      sx={{
+        '& .MuiDrawer-paper': {
+          width: '100vw',
+        },
+      }}
+    >
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: '100%', position: 'relative' }}
+        spacing={2}
+      >
+        <div style={{ position: 'absolute', top: 15, right: 2 }}>
+          <IconButton onClick={() => props.setOpenDrawer(false)}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <Grid item>
+          <Link to="/about" onClick={() => props.setOpenDrawer(false)}>
+            <Typography variant="h5">About</Typography>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/skills" onClick={() => props.setOpenDrawer(false)}>
+            <Typography variant="h5">Skills</Typography>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/project" onClick={() => props.setOpenDrawer(false)}>
+            <Typography variant="h5">Project</Typography>
+          </Link>
+        </Grid>
+      </Grid>
+    </SwipeableDrawer>
+  );
+  if (matchesSM) {
+    return mobile;
+  }
   return (
     <Grid container direction="column">
       <Grid
         item
         container
         justifyContent="end"
-
         // alignItems="center"
         style={{
-          paddingRight: !matchesSM && "8em",
-          paddingTop: matchesSM ? "2em" : "2em",
+          paddingRight: matchesSM ? 0 : '8em',
+          paddingTop: matchesSM ? '2em' : '2em',
           // border: "2px solid blue",
-          display: "flex",
+          display: 'flex',
           // justifyContent: "flex-end"
         }}
       >
@@ -32,45 +89,49 @@ export default function AppBar() {
           item
           xs={10}
           md={8}
-          style={{
-            // border: "2px solid blue",
-            // display: "flex",
-            // justifyContent: "center",
-            // alignItems: "center"
-          }}
+          style={
+            {
+              // border: "2px solid blue",
+              // display: "flex",
+              // justifyContent: "center",
+              // alignItems: "center"
+            }
+          }
         >
           <Card
             style={{
-              boxShadow: "rgba(100,100,111,0.2) 0px 7px 29px 0px",
+              boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
               //boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-              padding: matchesSM ? "3em 1em" : "1.5em 2em",
-              backgroundColor: "#F6F5FA",
+              padding: matchesSM ? '3em 1em' : '1.5em 2em',
+              backgroundColor: '#F6F5FA',
               borderRadius: 20,
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              paddingRight: "3em",
-              paddingLeft: "3em"
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingRight: '3em',
+              paddingLeft: '3em',
             }}
           >
             {/* first card */}
             <Card
               style={{
-                cursor: "pointer",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "16%",
+                cursor: 'pointer',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '16%',
                 // boxShadow: "9px 9px 16px rgb(163, 177, 198, 0.6), -9px -9px 16px  rgba(255,255,255, 0.5)",
-                boxShadow: "rgba(100,100,111,0.2) 0px 7px 29px 0px",
+                boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
                 //boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                padding: matchesSM ? "3em 1em" : "0.2em 0",
-                backgroundColor: "#F6F5FA",
+                padding: matchesSM ? '3em 1em' : '0.2em 0',
+                backgroundColor: '#F6F5FA',
                 borderRadius: 20,
               }}
             >
               <Typography variant="body1">
-                <Link to="/about" style={{ textDecoration: "none" }}>About</Link>
+                <Link to="/about" style={{ textDecoration: 'none' }}>
+                  About
+                </Link>
                 {/* About */}
               </Typography>
             </Card>
@@ -78,47 +139,49 @@ export default function AppBar() {
             {/* second card */}
             <Card
               style={{
-                cursor: "pointer",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "16%",
-                boxShadow: "rgba(100,100,111,0.2) 0px 7px 29px 0px",
+                cursor: 'pointer',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '16%',
+                boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
                 //boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                padding: matchesSM ? "3em 1em" : "0.2em 0",
-                backgroundColor: "#F6F5FA",
+                padding: matchesSM ? '3em 1em' : '0.2em 0',
+                backgroundColor: '#F6F5FA',
                 borderRadius: 20,
               }}
             >
               <Typography>
-                <Link to="/project" style={{ textDecoration: "none" }}>Project</Link>
-
+                <Link to="/project" style={{ textDecoration: 'none' }}>
+                  Project
+                </Link>
               </Typography>
             </Card>
 
             {/* third card */}
             <Card
               style={{
-                cursor: "pointer",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "16%",
-                boxShadow: "rgba(100,100,111,0.2) 0px 7px 29px 0px",
+                cursor: 'pointer',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '16%',
+                boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
                 //boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                padding: matchesSM ? "3em 1em" : "0.2em 0",
-                backgroundColor: "#F6F5FA",
+                padding: matchesSM ? '3em 1em' : '0.2em 0',
+                backgroundColor: '#F6F5FA',
                 borderRadius: 20,
               }}
             >
               <Typography>
-                <Link to="/skills" style={{ textDecoration: "none" }}>Skills</Link>
+                <Link to="/skills" style={{ textDecoration: 'none' }}>
+                  Skills
+                </Link>
               </Typography>
             </Card>
-
           </Card>
         </Grid>
       </Grid>
-    </Grid >
+    </Grid>
   );
 }
