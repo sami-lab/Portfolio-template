6fce0eb99891c11e77350c307550b96721a37244
muhammadsalif <muhammad.salif@yahoo.com>
2021-09-24 20:11:43 +0500

    fixed.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import ProTip from "./ProTip";
 import About from "./pages/about";
-import { Grid, Paper } from "@mui/material";
-import logo from "./assets/ed.png"
-import profile from "./assets/profile.jpg"
-import map from "./assets/map.png"
+import { Grid, Paper, Button, Typography } from "@mui/material";
 import Skills from "./pages/skills";
 import Project from "./pages/project"
+
+import profile from "./assets/profile.jpg"
+import logo from "./assets/ed.png"
+import map from "./assets/map.png"
+import hand from "./assets/hand.gif"
+import location from "./assets/location.jpg"
+import book from "./assets/book.png"
 
 export default function App() {
   return (
@@ -23,20 +26,173 @@ export default function App() {
         </div>
 
 
-        {/* left paper */}
-        <Paper style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        {/* left div */}
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
 
-          {/* first image div */}
-          <Paper>
-            <img src={profile} style={{ width: "90px", height: "90px" }} />
+          <Paper
+            elevation={0}
+            style={{
+              borderRadius: 20,
+              paddingBottom: "1em",
+              boxShadow: "rgba(100,100,111,0.2) 0px 7px 29px 0px",
+              //boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+              width: "50%",
+              marginTop: "2em",
+              backgroundColor: "#F6F5FA",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column"
+            }}
+          >
+
+            {/* first image div */}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: "1em 0px",
+                overflow: "hidden",
+              }}
+            >
+              <span>
+                <img
+                  src={hand}
+                  style={{
+                    // border: "3px solid white",
+                    // borderRadius: 20,
+                    width: "50px",
+                    height: "50px"
+                  }}
+                />
+              </span>
+              <img
+                src={profile}
+                style={{
+                  border: "3px solid white",
+                  borderRadius: 20,
+                  width: "100px",
+                  height: "85px"
+                }}
+              />
+
+            </div>
+
+            {/* second image div */}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "start",
+                margin: "1em 0px",
+                overflow: "hidden",
+              }}
+            >
+              <span>
+                <img
+                  src={location}
+                  style={{
+                    // border: "3px solid white",
+                    // borderRadius: 20,
+                    width: "50px",
+                    height: "50px"
+                  }}
+                />
+              </span>
+              <img
+                src={map}
+                style={{
+                  border: "3px solid white",
+                  borderRadius: 20,
+                  width: "100px",
+                  height: "85px"
+                }}
+              />
+            </div>
+
+            {/* third image div */}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "start",
+                margin: "1em 0px",
+                overflow: "hidden",
+              }}
+            >
+              <span>
+                <img
+                  src={book}
+                  style={{
+                    // border: "3px solid white",
+                    // borderRadius: 20,
+                    width: "30px",
+                    height: "30px",
+                    marginTop: "1em"
+                  }}
+                />
+              </span>
+
+              <div
+                style={{
+                  // border: "3px solid white",
+                  backgroundColor: "#EDF2F8",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 20,
+                  flexDirection: "column",
+                  width: "120px",
+                  height: "70px"
+                }}
+              >
+                <Typography variant="body1">
+                  hello
+                </Typography>
+
+                <Typography variant="body1">
+                  icon here
+                  Software Enginer
+                </Typography>
+
+              </div>
+
+            </div>
+
+            {/* fourth image div */}
+            <div style={{
+              margin: "1em 0px",
+              borderRadius: 20,
+              overflow: "hidden",
+              // border: "3px solid white",
+              backgroundColor: "#EDF2F8",
+              width: "130px",
+              height: "85px"
+            }}
+            >
+              {/* <img src={map} style={{ width: "100px", height: "85px" }} /> */}
+
+            </div>
+
+            {/* bottom button */}
+            <div style={{
+              marginTop: "1em",
+              // borderRadius: 20,
+              // overflow: "hidden",
+              // border: "3px solid white"
+            }}
+            >
+              <Button style={{ backgroundColor: "white" }}>
+                <Typography variant="body1">
+                  View Resume
+                </Typography>
+              </Button>
+            </div>
+
           </Paper>
 
-          {/* second image Paper */}
-          <Paper>
-            <img src={map} style={{ width: "90px", height: "90px" }} />
-          </Paper>
-
-        </Paper>
+        </div>
 
       </Grid>
 
