42c3134aa64ed8a67b232d564b1f45366bf6c356
Sami <s.m.sami125@gmail.com>
2021-09-25 01:15:50 +0500

    session padding

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { Card, Grid, Typography, useTheme, useMediaQuery } from "@mui/material";
+import React from 'react';
+import { Card, Grid, Typography, useTheme, useMediaQuery } from '@mui/material';
 
-import Man from "../assets/man.jpg";
+import Man from '../assets/man.jpg';
 export default function About() {
   const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
   return (
     <Grid container direction="column">
       <Grid
@@ -12,15 +12,15 @@ export default function About() {
         container
         justifyContent="center"
         alignItems="center"
-        style={{ paddingTop: matchesSM ? "2em" : "5em" }}
+        style={{ paddingTop: matchesSM ? '2em' : '3em' }}
       >
         <Grid item xs={10}>
           <Card
             style={{
-              boxShadow: "rgba(100,100,111,0.2) 0px 7px 29px 0px",
+              boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
               //boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-              padding: matchesSM ? "3em 1em" : "6em 2em",
-              backgroundColor: "#F6F5FA",
+              padding: matchesSM ? '3em 1em' : '4em 2em',
+              backgroundColor: '#F6F5FA',
               borderRadius: 20,
             }}
           >
@@ -28,7 +28,7 @@ export default function About() {
             <Grid
               container
               justifyContent="center"
-              wrap={matchesSM ? "wrap" : "nowrap"}
+              wrap={matchesSM ? 'wrap' : 'nowrap'}
             >
               {/* For image */}
               <Grid item>
@@ -40,7 +40,7 @@ export default function About() {
                   <Grid item>
                     <Typography variant="body1">Hello, I'm Jphnns</Typography>
                   </Grid>
-                  <Grid item style={{ marginLeft: "4.5em" }}>
+                  <Grid item style={{ marginLeft: '4.5em' }}>
                     <Typography variant="h4">Welcome to my World!</Typography>
                   </Grid>
                   <Grid item container justifyContent="center">
@@ -48,8 +48,8 @@ export default function About() {
                       variant="body2"
                       color="#2978B5"
                       style={{
-                        width: matchesSM ? "80%" : "50%",
-                        marginTop: "2em",
+                        width: matchesSM ? '80%' : '50%',
+                        marginTop: '2em',
                       }}
                     >
                       Adipisicing sit fugit ullam unde aliquid sequi Facilis
